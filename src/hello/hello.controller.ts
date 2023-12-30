cde426a9b537202b5eed29c5cf1e15d5c52f2ccd
vinod <vinodk2757@gmail.com>
2023-12-30 22:35:12 +0530

    update file path fix unit test

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Injectable } from '@nestjs/common';
 import { Public } from '../auth/decorators/public.decorator';
 import { HttpCode } from '@nestjs/common';
-import { LoggerService } from '@nestjs/common/services/logger.service';
+import { LoggerService } from '../common/service/logger.service';
 import { v4 as uuid } from 'uuid';
 
 
+@Injectable()
 @Controller('')
 export class HelloController {
-  private readonly logger: LoggerService
+  constructor(
+    private readonly logger: LoggerService
+  ) {}
 
     @Public()
     @HttpCode(200) 
     @Get()
-    helloWorld(): string {
+    async helloWorld(): Promise<string> {
       const id: string = uuid();
       this.logger.log('User login api called',id,'hello.controler.ts','GET','/','helloWorld');
       return 'Hello World!';
