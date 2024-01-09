7811211d21bd7a257663c5ab29925588f160491a
vinod <vinodk2757@gmail.com>
2024-01-09 22:47:26 +0530

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
-import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
+import helmet from "helmet";
+import * as cookieParser from "cookie-parser";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
@@ -16,27 +16,31 @@ async function bootstrap() {
     .addServer("https://staging.yourapi.com/", "Staging")
     .addServer("https://production.yourapi.com/", "Production")
     .addTag("Jwt authentication")
-    .addCookieAuth('refresh_token', {
-      type: 'apiKey',
-      in: 'cookie'
-    },
-    'refresh_token')
+    .addCookieAuth(
+      "refresh_token",
+      {
+        type: "apiKey",
+        in: "cookie",
+      },
+      "refresh_token"
+    )
     .addBearerAuth(
       {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+        description: "Enter JWT token",
+        in: "header",
       },
-      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      "JWT-auth" // This name here is important for matching up with @ApiBearerAuth() in your controller!
     )
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("api", app, document);
   app.use(helmet());
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(Number(process.env.PORT) || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
